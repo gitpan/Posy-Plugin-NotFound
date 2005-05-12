a68266d93eb5130d6c7f4c40d38eb671389f5805
Kathryn Andersen <rubykat@cpan.org>
2005-05-12 11:49:53 +0000

    Import of RUBYKAT/Posy-Plugin-NotFound-0.22 from CPAN.
    gitpan-cpan-distribution: Posy-Plugin-NotFound
    gitpan-cpan-version:      0.22
    gitpan-cpan-path:         RUBYKAT/Posy-Plugin-NotFound-0.22.tar.gz
    gitpan-cpan-author:       RUBYKAT
    gitpan-cpan-maturity:     released

--- lib/Posy/Plugin/NotFound.pm
+++ lib/Posy/Plugin/NotFound.pm
@@ -7,11 +7,11 @@ Posy::Plugin::NotFound - Posy plugin to provide a custom Not Found page.
 
 =head1 VERSION
 
-This describes version B<0.21> of Posy::Plugin::NotFound.
+This describes version B<0.22> of Posy::Plugin::NotFound.
 
 =cut
 
-our $VERSION = '0.21';
+our $VERSION = '0.22';
 
 =head1 SYNOPSIS
 
